7adbde1824afa45d10a0fce63af73f76f0e75e86
Gediminas Zakas <gediminaszakas@gmail.com>
2023-03-15 20:24:33 +0200

    added_camera_movement

--- level.py
+++ level.py
@@ -8,13 +8,10 @@ from debug import debug
 class Level:
     def __init__(self):
         
-        # get the display surface
         self.display_surface = pygame.display.get_surface()
-        # sprite group setup
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacles_sprites = pygame.sprite.Group()
 
-        # sprite setup
         self.create_map()
 
     def create_map(self):
@@ -30,7 +27,21 @@ class Level:
 
 
     def run(self):
-        # update and draw the game
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self, player):
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+        for sprite in self.sprites():
+            offset_position = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_position)
--- player.py
+++ player.py
@@ -4,7 +4,7 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, obstacle_sprites):
         super().__init__(groups)
-        self.image = pygame.image.load(os.path.join('graphics', 'player.png')).convert_alpha()
+        self.image = pygame.image.load(os.path.join('sprites', 'player.png')).convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
 
         self.direction = pygame.math.Vector2()
