77ba97b5ef9b6d7f0bcf3f4bc2cb674b186009d9
Gediminas Zakas <gediminaszakas@gmail.com>
2023-03-15 19:12:41 +0200

    fixed_diagonal_movement_speed

--- player.py
+++ player.py
@@ -8,7 +8,7 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft = pos)
 
         self.direction = pygame.math.Vector2()
-        self.speed = 5
+        self.speed = 2.5
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -28,6 +28,8 @@ class Player(pygame.sprite.Sprite):
             self.direction.y = 0
 
     def move(self, speed):
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
         self.rect.center += self.direction * speed
 
     def update(self):
