ad0e3c3ba38e9b35c398b9695b933c09a060726c
Gediminas Zakas <gediminaszakas@gmail.com>
2023-03-15 19:07:50 +0200

    added_player_movement

--- level.py
+++ level.py
@@ -2,6 +2,7 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
+from debug import debug
 
 
 class Level:
@@ -24,10 +25,12 @@ class Level:
                 if column == 'x':
                     Tile((x, y), [self.visible_sprites, self.obstacles_sprites])
                 if column == 'p':
-                    Player((x, y), [self.visible_sprites])
+                    self.player = Player((x, y), [self.visible_sprites])
 
 
 
     def run(self):
         # update and draw the game
         self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
--- player.py
+++ player.py
@@ -5,5 +5,31 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
         self.image = pygame.image.load(os.path.join('graphics', 'player.png')).convert_alpha()
-        self.image_rect = self.image.get_rect()
         self.rect = self.image.get_rect(topleft = pos)
+
+        self.direction = pygame.math.Vector2()
+        self.speed = 5
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_d]:
+            self.direction.x = 1
+        elif keys[pygame.K_a]:
+            self.direction.x =  -1
+        else:
+            self.direction.x = 0
+
+        if keys[pygame.K_w]:
+            self.direction.y = -1
+        elif keys[pygame.K_s]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+    def move(self, speed):
+        self.rect.center += self.direction * speed
+
+    def update(self):
+        self.input()
+        self.move(self.speed)
